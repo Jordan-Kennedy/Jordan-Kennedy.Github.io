0cccc9b207e3a163b0b6fda87602d0750731b71a
Jordan-Kennedy <35365534+Jordan-Kennedy@users.noreply.github.com>
2018-01-15 09:33:15 +0000

    Update and rename Updates.html to Updates.php
    Changing the document type to .php instead of .html so I can use navigation.php as my navigation bar.

---
+++ Updates.php
@@ -0,0 +1,18 @@
+<!DOCTYPE php>
+<php>
+<head>
+	<link href="Jordan's forum CSS.css" rel="stylesheet">
+	<title>Updates</title>
+	<meta charset="UTF-8">
+	<style>
+		ul {}
+	</style>
+</head>
+  <body>
+	<h1>Jordan's forum Updates</h1>
+	<ul>
+		<?php include("includes/navigation.php");?>
+	</ul>
+	<p>Coming soon</p>
+  </body>
+</html>
