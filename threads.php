41318ac88e8a5214df7fc560973c0f678b71f6c5
Jordan-Kennedy <35365534+Jordan-Kennedy@users.noreply.github.com>
2018-01-15 09:23:04 +0000

    Update threads.php
    Implementing php to handle the menu, this means i will only have to change the one document instead of multiple which will save me a lot of time in the future.

--- threads.php
+++ threads.php
@@ -1,22 +1,16 @@
-<!DOCTYPE html>
-<html>
+<!DOCTYPE php>
+<php>
 <head>
 	<link href="Jordan's forum CSS.css" rel="stylesheet">
 	<title>The Forums</title>
 	<meta charset="UTF-8">
 	<style>
-		ul {}
+		/*ul {}*/
 	</style>
 </head>
   <body>
 	<h1>Threads</h1>
-	<ul>
-		<li><a href="index.html">Home</a></li>
-		<li><a class="active" href="threads.html">Threads</a></li>
-		<li><a href="updates.html">Updates</a></li>
-		<li><a href="contact.html">Contact</a></li>
-		<li><a href="about.html">About</a></li>
-		<li><a href="help.html">Help</a></li>
+		<?php include("includes/navigation.php");?>
 	</ul>
 	<p>Coming soon</p>
   </body>
