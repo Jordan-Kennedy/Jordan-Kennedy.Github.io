5a5f875090e1e2e8b99e02e2a2a58622327de202
Jordan-Kennedy <35365534+Jordan-Kennedy@users.noreply.github.com>
2018-01-15 09:19:51 +0000

    Create navigation.php

---
+++ navigation.php
@@ -0,0 +1,14 @@
+<!DOCTYPE php>
+<php>
+<style>
+		ul {}
+    <ul>
+      <li><a href="index.html">Home</a></li>
+		  <li><a href="threads.html">Threads</a></li>
+		  <li><a href="updates.html">Updates</a></li>
+		  <li><a href="contact.html">Contact</a></li>
+		  <li><a href="about.html">About</a></li>
+		  <li><a href="help.html">Help</a></li>
+	  </ul>
+  </style>
+  </php>
